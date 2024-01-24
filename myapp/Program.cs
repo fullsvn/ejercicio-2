9c69ba48c9c9e5e503ef28b1ca8e7421c91e8249
fullsvn <119902654+fullsvn@users.noreply.github.com>
2024-01-24 18:22:38 -0400

    Add files via upload

---
+++ myapp/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Ingrese un año: ");
+        int year = int.Parse(Console.ReadLine());
+
+        // Llamar a la función para verificar si el año es bisiesto
+        if (EsBisiesto(year))
+        {
+            Console.WriteLine($"{year} es un año bisiesto.");
+        }
+        else
+        {
+            Console.WriteLine($"{year} no es un año bisiesto.");
+        }
+
+        
+
+static bool EsBisiesto(int year)
+    {
+        // Un año es bisiesto si es divisible por 4
+        // Pero no es bisiesto si es divisible por 100, a menos que también sea divisible por 400
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    }
+
+
+
+
